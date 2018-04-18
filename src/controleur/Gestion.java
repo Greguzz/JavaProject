8e3aaaed96748d9eafcfb78e08ffa270fa1c45b2
GREYGOOSE\Grégoire <gregoired2c@gmail.com>
2018-04-18 14:35:12 +0200

    Commit Gestion.java

--- src/controleur/Gestion.java
+++ src/controleur/Gestion.java
@@ -5,6 +5,9 @@
  */
 package controleur;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
 /**
  *
  * @author Manu
@@ -14,11 +17,15 @@ public class Gestion {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        //commentaire 
-        //lol
-        ///DJFEABFJHEBJ
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        System.out.println("Veuillez entrer votre identifiant");
+        Scanner sc = new Scanner(System.in);
+        String id=sc.next();
+        System.out.println("Veuillez entrer votre mot de passe");
+        String password= sc.next();
+        System.out.println("Veuillez entrer le nom de la base de donnée");
+        String db = sc.next();
+        Connexion C = new Connexion(db, id, password);
     }
     
 }
