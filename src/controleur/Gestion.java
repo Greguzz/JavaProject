59564057b8c04b8346f4c708335a28cffc669412
GREYGOOSE\Grégoire <gregoired2c@gmail.com>
2018-04-18 16:03:12 +0200

    Gestion.java

--- src/controleur/Gestion.java
+++ src/controleur/Gestion.java
@@ -6,6 +6,7 @@
 package controleur;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import vue.Graphique;
 
@@ -54,8 +55,7 @@ public class Gestion {
                         System.out.println("Directeur ?");
                         tab[3]=sc.next();
                         requete = "SELECT * FROM service WHERE code LIKE" +tab[0] + "AND nom LIKE "+ tab[1] + "AND batiment LIKE" + tab[2] + "AND directeur LIKE" + tab[3];
-                        
-                
+                        C.executeUpdate(requete);
                 
                 }
                 
